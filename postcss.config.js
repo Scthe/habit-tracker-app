d3599c21cf29f7305bff1ab0844377efc22a1575
Scthe <marcin1113C@gmail.com>
2021-04-27 01:23:19 +0200

    init

---
+++ postcss.config.js
@@ -0,0 +1,14 @@
+module.exports = {
+  plugins: [
+    require("postcss-import"),
+    require("precss"),
+    require("postcss-normalize"),
+    require("autoprefixer"),
+    ["postcss-url",
+      // { filter: '**/assets/copy/*.png', url: 'copy', assetsPath: 'img', useHash: true },
+      // { filter: '**/fontsource/*', url: 'copy', assetsPath: 'fonts' },
+      // { url: "inline" }
+      { url: "rebase" }
+    ],
+  ],
+};
